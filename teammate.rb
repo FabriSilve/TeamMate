23c2457abf0189f47f074044e974671058ae75e9
Fabrizio Silvestri <fabrizio.silvestri@yahoo.it>
2020-04-09 10:25:01 +0200

    [Release] Version 1.4

--- teammate.rb
+++ teammate.rb
@@ -1,9 +1,9 @@
 class Teammate < Formula
   desc "A script to create and share faster github pull requests in your team"
   homepage "https://fabrisilve.github.io/TeamMate/"
-  url "https://github.com/FabriSilve/TeamMate/raw/master/teammate-1.3.tar.gz"
-  sha256 "bdb9c7b1c4ddba3a0e20a26f605da1530d02e0ab290db10dccf932b7ab4d6387"
-  version "1.3"
+  url "https://github.com/FabriSilve/TeamMate/raw/master/teammate-1.4.tar.gz"
+  sha256 "0a0534aa5af5182d3e0ed0904b5e5a0e8eecf758b1f4d54e73f9a5f4f4ca6f85"
+  version "1.4"
 
   depends_on "hub"
   depends_on "git"
